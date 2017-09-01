a39df74b831e964ccedf159d02e865d17c0771b6
Ben Chadwick <ben.chadwick@dublin.siteserv.ie>
2017-09-01 14:18:20 +0100

    initial proj structure

---
+++ investment_data.py
@@ -0,0 +1,67 @@
+
+current_stocks = [
+    {
+        "name": "Disney",
+        "amount_usd": "109.99",
+        "fees_usd": "2.99",
+        "type": "Shares",
+        "provider": "DriveWealth",
+        "start_date": "30/01/2017",
+        "next_review": "14/01/2018"
+    },
+    {
+        "name": "Snap",
+        "amount_usd": "50",
+        "fees_usd": "2.99",
+        "type": "Shares",
+        "provider": "DriveWealth",
+        "start_date": "15/08/2017",
+        "next_review": "11/02/2018"
+    },
+    {
+        "name": "Arena",
+        "amount_usd": "4.2",
+        "fees_usd": "2.99",
+        "type": "Shares",
+        "provider": "DriveWealth",
+        "start_date": "13/06/2017",
+        "next_review": "10/12/2017"
+    },
+    {
+        "name": "Under Armour",
+        "amount_usd": "32.83",
+        "fees_usd": "3.98",
+        "type": "Shares",
+        "provider": "DriveWealth",
+        "start_date": "30/01/2017",
+        "next_review": "14/01/2018"
+    },
+    {
+        "name": "FitBit",
+        "amount_usd": "100",
+        "fees_usd": "2.99",
+        "type": "Shares",
+        "provider": "DriveWealth",
+        "start_date": "22/02/2017",
+        "next_review": "11/02/2017"
+    },
+    {
+        "name": "Twitter",
+        "amount_usd": "97.01",
+        "fees_usd": "2.99",
+        "type": "Shares",
+        "provider": "DriveWealth",
+        "start_date": "28/04/2017",
+        "next_review": "08/10/2017"
+    },
+    {
+        "name": "TakeTwo",
+        "amount_usd": "54",
+        "fees_usd": "2.99",
+        "type": "Shares",
+        "provider": "DriveWealth",
+        "start_date": "30/01/2017",
+        "next_review": "14/01/2018"
+    }
+]
+
