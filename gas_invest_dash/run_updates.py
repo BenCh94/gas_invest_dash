07d29e5e8be690be1ababf17efc3720f5ef40784
Ben <bchadwick94@gmail.com>
2018-05-22 17:57:40 +0100

    Updated script path and function code

--- gas_invest_dash/run_updates.py
+++ gas_invest_dash/run_updates.py
@@ -1,9 +1,11 @@
 from gas_invest_dash.stock_data import *
 
+
 def update_stocks():
+	"""Function to update shares from the IEX API"""
 	get_iex_sandp()
-    for share in db_models.Share.objects:
-        if share.status == 'Inactive':
-            continue
-        iex_stock_chart(share.name)
-    return 'Complete'
+	for share in db_models.Share.objects:
+		if share.status == 'Inactive':
+			continue
+		iex_stock_chart(share.name)
+	return 'Complete'
